9f4d062490069c2b62c1fbccd93176e2892dbd1b
Steven McCauley <steven_mccauley@brown.edu>
2017-05-19 12:19:45 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ install_reqs = [
 	]
 # reqs is a list of requirement
 # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-reqs = [str(ir.req) for ir in install_reqs]
+#reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='wos-client',
